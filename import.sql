56ba71acc68e9cd488c9eeca4334a59acda12b86
David <unbekannt.ghost@gmail.com>
2022-02-07 09:00:58 +0100

    add basic files and docker-compose file

---
+++ import.sql
@@ -0,0 +1,11 @@
+create table kunde(
+    IdKunde int,
+    Name varchar(50),
+    Postleitzahl int(60),
+    OrtName varchar(40),
+    Arbeitgeber varchar(20)
+);
+
+DESCRIBE kunde;
+
+select * from KUNDE;
